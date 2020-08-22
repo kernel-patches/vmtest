486e8adaecd14883ef6b8753ac10397e1ba4033e
Andrii Nakryiko <andriin@fb.com>
2020-08-21 19:06:43 -0700

    vmtests: add log folding
    Cherry-picks
    https://github.com/libbpf/libbpf/commit/4001a658e05f4385cc7764bbe0e6a110be26a468
    
    Sprinkle log folds around, including timing.
    
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>

--- travis-ci/vmtest/run_vmtest.sh
+++ travis-ci/vmtest/run_vmtest.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-travis_fold(){
-  echo -ne "travis_fold:start:$1\\r"
-}
-travis_unfold(){
-  echo -ne "travis_fold:end:$1\\r"
-}
+set -eu
 
-set -eux
+source $(cd $(dirname $0) && pwd)/helpers.sh
 VMTEST_SETUPCMD="PROJECT_NAME=${PROJECT_NAME} ./${PROJECT_NAME}/travis-ci/vmtest/run_selftests.sh"
 echo "KERNEL: $KERNEL"
+echo
 
-travis_fold "Build_latest_pahole"
 ${VMTEST_ROOT}/build_pahole.sh travis-ci/vmtest/pahole
-travis_unfold "Build_latest_pahole"
 
-# Install required packagesi
-travis_fold "Install_latest_clang"
+# Install required packages
+travis_fold start install_clang "Installing Clang/LLVM"
 n=0
 while [ $n -lt 5 ]; do
   set +e && \
@@ -32,17 +26,13 @@ if [ $n -ge 5 ] ; then
   echo "clang install failed"
   exit 1
 fi
-travis_unfold "Install_latest_clang"
-
+travis_fold end install_clang
 
 # Build selftests (and latest kernel, if necessary)
-travis_fold "Build_kernel_and_selftests"
 KERNEL="${KERNEL}" ${VMTEST_ROOT}/prepare_selftests.sh
-travis_unfold "Build_kernel_and_selftests"
 
 
 
-travis_fold "Run_selftests_in_VM"
 # Escape whitespace characters.
 setup_cmd=$(sed 's/\([[:space:]]\)/\\\1/g' <<< "${VMTEST_SETUPCMD}")
 sudo adduser "${USER}" kvm
@@ -52,4 +42,3 @@ if [[ "${KERNEL}" = 'LATEST' ]]; then
 else
   sudo -E sudo -E -u "${USER}" "${VMTEST_ROOT}/run.sh" -k "${KERNEL}*" -o -d ~ -s "${setup_cmd}" ~/root.img;
 fi
-travis_unfold "Run_selftests_in_VM"
