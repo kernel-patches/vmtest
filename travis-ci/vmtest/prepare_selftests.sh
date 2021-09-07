be2b641f64b6fc947bc99ba8f8f4f6cf0d6f1bee
Yucong Sun <sunyucong@gmail.com>
2021-09-07 15:05:11 -0400

    Apply local patches before building kernel

--- travis-ci/vmtest/prepare_selftests.sh
+++ travis-ci/vmtest/prepare_selftests.sh
@@ -4,6 +4,14 @@ set -eu
 
 source $(cd $(dirname $0) && pwd)/helpers.sh
 
+travis_fold start apply_local_patch "Apply local patches"
+if ls ${VMTEST_ROOT}/patches/*.patch 1>/dev/null 2>&1; then
+  for file in ${VMTEST_ROOT}/patches/*.patch; do
+    patch -p1 < ${file}
+  done
+fi
+travis_fold end apply_local_patch
+
 travis_fold start build_kernel "Kernel build"
 cp ${VMTEST_ROOT}/configs/latest.config .config
 make -j $((4*$(nproc))) olddefconfig all
