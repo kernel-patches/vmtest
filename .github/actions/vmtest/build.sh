af2e5181d98a22d3cc9de99a994795aca4cc0de9
Yucong Sun <sunyucong@gmail.com>
2022-02-07 10:50:27 -0500

    Build kernel with clang

--- .github/actions/vmtest/build.sh
+++ .github/actions/vmtest/build.sh
@@ -9,6 +9,6 @@ source "${THISDIR}"/helpers.sh
 travis_fold start build_kernel "Building kernel"
 
 cp "${GITHUB_ACTION_PATH}"/latest.config .config
-make -j $((4*$(nproc))) olddefconfig all > /dev/null
+make -j LLVM=1 $((4*$(nproc))) olddefconfig all > /dev/null
 
 travis_fold end build_kernel
