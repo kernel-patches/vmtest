5a6ece21c6805e0d373ea076ca79dd9dac4eb5e9
Yucong Sun <sunyucong@gmail.com>
2022-02-17 14:45:25 -0500

    Update build.sh

--- .github/actions/vmtest/build.sh
+++ .github/actions/vmtest/build.sh
@@ -13,11 +13,8 @@ cp "${GITHUB_ACTION_PATH}"/latest.config .config
 LLVM_VER=15
 
 make LLVM=1 \
-  LD=lld-${LLVM_VER} HOSTLD=lld-${LLVM_VER} \
-	CLANG=clang-${LLVM_VER} \
-	LLC=llc-${LLVM_VER} \
-	LLVM_STRIP=llvm-strip-${LLVM_VER} \
-  LD=ld.lld-15 HOSTLD=ld.lld-15 \
+  CC=clang-${LLVM_VER} HOSTCC=clang-${LLVM_VER}\
+  LD=ld.lld-${LLVM_VER} HOSTLD=ld.lld-${LLVM_VER} \
   -j $((4*$(nproc))) olddefconfig all > /dev/null
 
 travis_fold end build_kernel
