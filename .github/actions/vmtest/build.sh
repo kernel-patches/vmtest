28a5721032804e2fd22a60c71a786ac95cf807ea
Mykola Lysenko <mykolal@fb.com>
2022-03-16 12:01:45 -0700

    Add support for testing on s390x ibm setup

--- .github/actions/vmtest/build.sh
+++ .github/actions/vmtest/build.sh
@@ -2,13 +2,16 @@
 
 set -euo pipefail
 
+ARCH="$1"
+
 THISDIR="$(cd $(dirname $0) && pwd)"
 
 source "${THISDIR}"/helpers.sh
 
 travis_fold start build_kernel "Building kernel"
 
-cp "${GITHUB_ACTION_PATH}"/latest.config .config
+cp ${GITHUB_WORKSPACE}/travis-ci/vmtest/configs/config-latest.${ARCH} .config
+
 make -j $((4*$(nproc))) olddefconfig all > /dev/null
 
 travis_fold end build_kernel
