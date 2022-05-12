d9cc88183ff36b9137c08af845b1555bf4e2f1de
Mykola Lysenko <mykolal@fb.com>
2022-05-11 18:16:39 -0700

    ci: add LLVM build support
    Enable kernel and BPF selftests build with LLVM
    toolchain. Pass toolchain name and version to
    prepare infra to build with different LLVM toolchain
    versions. When using GCC toolchain, we will use LLVM
    toolchain version 15 by default.
    
    Signed-off-by: Mykola Lysenko <mykolal@fb.com>

--- .github/actions/vmtest/build.sh
+++ .github/actions/vmtest/build.sh
@@ -3,12 +3,19 @@
 set -euo pipefail
 
 ARCH="$1"
+TOOLCHAIN="$2"
+TOOLCHAIN_NAME="$(echo $TOOLCHAIN | cut -d '-' -f 1)"
+TOOLCHAIN_VERSION="$(echo $TOOLCHAIN | cut -d '-' -f 2)"
+
+if [ "$TOOLCHAIN_NAME" == "llvm" ]; then
+export LLVM="-$TOOLCHAIN_VERSION"
+fi
 
 THISDIR="$(cd $(dirname $0) && pwd)"
 
 source "${THISDIR}"/helpers.sh
 
-travis_fold start build_kernel "Building kernel"
+travis_fold start build_kernel "Building kernel with $TOOLCHAIN"
 
 cp ${GITHUB_WORKSPACE}/travis-ci/vmtest/configs/config-latest.${ARCH} .config
 
