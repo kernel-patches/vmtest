512a4299ea9403f0897a67ba780ecff9fcd19032
Manu Bretelle <chantr4@gmail.com>
2023-03-25 00:44:58 -0700

    `black`en script
    Signed-off-by: Manu Bretelle <chantr4@gmail.com>

--- ci/scripts/get_test_matrix.py
+++ ci/scripts/get_test_matrix.py
@@ -6,18 +6,22 @@ import argparse
 from enum import Enum
 from json import dumps
 
+
 class Arch(Enum):
     """
     CPU architecture supported by CI.
     """
+
     aarch64 = "aarch64"
     s390x = "s390x"
     x86_64 = "x86_64"
+
 
 def set_output(name, value):
     """Write an output variable to the GitHub output file."""
     with open(os.getenv("GITHUB_OUTPUT"), "a") as f:
         f.write(f"{name}={value}\n")
+
 
 def generate_test_config(test):
     """Create the configuration for the provided test."""
@@ -38,21 +42,54 @@ def generate_test_config(test):
     }
     return config
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--owner", required=True, help="Github owner")
     parser.add_argument("-r", "--repository", required=True, help="Github repository")
     return parser.parse_args()
 
+
 if __name__ == "__main__":
     args = parse_args()
 
     matrix = [
-        {"kernel": "LATEST", "runs_on": [], "arch": Arch.x86_64.value, "toolchain": "gcc", "llvm-version": "16"},
-        {"kernel": "LATEST", "runs_on": [], "arch": Arch.x86_64.value, "toolchain": "llvm", "llvm-version": "16"},
-        {"kernel": "LATEST", "runs_on": [], "arch": Arch.aarch64.value, "toolchain": "gcc", "llvm-version": "16"},
-        {"kernel": "LATEST", "runs_on": [], "arch": Arch.aarch64.value, "toolchain": "llvm", "llvm-version": "16"},
-        {"kernel": "LATEST", "runs_on": [], "arch": Arch.s390x.value, "toolchain": "gcc", "llvm-version": "16", "parallel_tests": False},
+        {
+            "kernel": "LATEST",
+            "runs_on": [],
+            "arch": Arch.x86_64.value,
+            "toolchain": "gcc",
+            "llvm-version": "16",
+        },
+        {
+            "kernel": "LATEST",
+            "runs_on": [],
+            "arch": Arch.x86_64.value,
+            "toolchain": "llvm",
+            "llvm-version": "16",
+        },
+        {
+            "kernel": "LATEST",
+            "runs_on": [],
+            "arch": Arch.aarch64.value,
+            "toolchain": "gcc",
+            "llvm-version": "16",
+        },
+        {
+            "kernel": "LATEST",
+            "runs_on": [],
+            "arch": Arch.aarch64.value,
+            "toolchain": "llvm",
+            "llvm-version": "16",
+        },
+        {
+            "kernel": "LATEST",
+            "runs_on": [],
+            "arch": Arch.s390x.value,
+            "toolchain": "gcc",
+            "llvm-version": "16",
+            "parallel_tests": False,
+        },
     ]
     self_hosted_repos = [
         "kernel-patches/bpf",
@@ -60,10 +97,10 @@ if __name__ == "__main__":
     ]
 
     for idx in range(len(matrix) - 1, -1, -1):
-        if matrix[idx]['toolchain'] == 'gcc':
-            matrix[idx]['toolchain_full'] = 'gcc'
+        if matrix[idx]["toolchain"] == "gcc":
+            matrix[idx]["toolchain_full"] = "gcc"
         else:
-            matrix[idx]['toolchain_full'] = 'llvm-' + matrix[idx]['llvm-version']
+            matrix[idx]["toolchain_full"] = "llvm-" + matrix[idx]["llvm-version"]
     # Only a few repository within "kernel-patches" use self-hosted runners.
     if args.owner != "kernel-patches" or args.repository not in self_hosted_repos:
         # Outside of those repositories, we only run on x86_64 GH hosted runners (ubuntu-latest)
@@ -91,10 +128,13 @@ if __name__ == "__main__":
         ]
         if config.get("parallel_tests", True):
             return tests
-        return [test for test in tests if not test.endswith("parallel") ]
+        return [test for test in tests if not test.endswith("parallel")]
 
-    test_matrix = {"include": [{**config, **generate_test_config(test)}
-        for config in matrix
-        for test in get_tests(config)
-    ]}
+    test_matrix = {
+        "include": [
+            {**config, **generate_test_config(test)}
+            for config in matrix
+            for test in get_tests(config)
+        ]
+    }
     set_output("test_matrix", dumps(test_matrix))
