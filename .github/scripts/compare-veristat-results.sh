872158ae8283a4c5910e01871c1a509264597f14
Nikolay Yurin <yurinnick@meta.com>
2023-09-13 14:42:21 -0700

    Fix veristat baseline cache save/restore
    Github cache has S3-like structure, where key is a bucket and files
    specified in the path argument makes its content.
    Due to that fact save and restore actions MUST have the same path
    argument.
    
    Signed-off-by: Nikolay Yurin <yurinnick@meta.com>

--- .github/scripts/compare-veristat-results.sh
+++ .github/scripts/compare-veristat-results.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ ! -f "${CACHE_RESULT_KEY}" ]]; then
-    echo "# No ${CACHE_RESULT_KEY} available" >> "${GITHUB_STEP_SUMMARY}"
+if [[ ! -f "${BASELINE_PATH}" ]]; then
+    echo "# No ${BASELINE_PATH} available" >> "${GITHUB_STEP_SUMMARY}"
 
-    echo "No ${CACHE_RESULT_KEY} available"
+    echo "No ${BASELINE_PATH} available"
     echo "Printing veristat results"
     cat "${VERISTAT_OUTPUT}"
 
@@ -13,6 +13,6 @@ fi
 selftests/bpf/veristat \
     --output-format csv \
     --emit file,prog,verdict,states \
-    --compare "${CACHE_RESULT_KEY}" "${VERISTAT_OUTPUT}" > compare.csv
+    --compare "${BASELINE_PATH}" "${VERISTAT_OUTPUT}" > compare.csv
 
 python3 ./.github/scripts/veristat-compare.py compare.csv
