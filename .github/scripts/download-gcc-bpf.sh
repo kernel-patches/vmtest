e8c5ba2e381983f0fb04d368827ffbb5381eb186
Ihor Solodrai <ihor.solodrai@linux.dev>
2026-01-21 17:40:11 -0800

    BPF CI: Switch to Debian-based kernel builders
    Use a custom Debian-base docker image for kernel build
    jobs (https://github.com/kernel-patches/runner/pull/94).
    
    Bump libbpf/ci actions to v4 (https://github.com/libbpf/ci/pull/210).
    
    Other nits:
      - set CROSS_COMPILE only for s390x
      - simplify Linux source download
      - remove setup docker/qemu logic
      - in gcc-bpf, install github cli on demand
    
    Signed-off-by: Ihor Solodrai <ihor.solodrai@linux.dev>

--- .github/scripts/download-gcc-bpf.sh
+++ .github/scripts/download-gcc-bpf.sh
@@ -7,6 +7,19 @@ INSTALL_DIR=$(realpath $2)
 
 cd /tmp
 
+if ! command -v gh &> /dev/null; then
+    # https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+    (type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+fi
+
 tag=$(gh release list -L 1 -R ${GCC_BPF_RELEASE_GH_REPO} --json tagName -q .[].tagName)
 if [[ -z "$tag" ]]; then
     echo "Could not find latest GCC BPF release at ${GCC_BPF_RELEASE_GH_REPO}"
@@ -27,4 +40,3 @@ rm -rf $INSTALL_DIR
 mv -v $dir $INSTALL_DIR
 
 cd -
-
