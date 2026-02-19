bc5217a2a5cb9a01739b94c0e9333408e9047e70
Puranjay Mohan <puranjay12@gmail.com>
2026-02-19 00:02:16 +0000

    Implement a workflow to run veristat on cilium programs (#441)
    * download-gcc-bpf.sh: Rename to download-gh-release.sh
    
    Make the script generic by renaming GCC-BPF-specific variable names so
    it can be reused by other workflows that need to download GitHub
    releases.
    
    * Implement a workflow to run veristat on cilium bpf programs
    
    Signed-off-by: Puranjay Mohan <puranjay@kernel.org>
    Signed-off-by: Ihor Solodrai <ihor.solodrai@linux.dev>

--- .github/scripts/download-gh-release.sh
+++ .github/scripts/download-gh-release.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-GCC_BPF_RELEASE_GH_REPO=$1
+GH_REPO=$1
 INSTALL_DIR=$(realpath $2)
 
 cd /tmp
@@ -20,13 +20,13 @@ if ! command -v gh &> /dev/null; then
 	&& sudo apt install gh -y
 fi
 
-tag=$(gh release list -L 1 -R ${GCC_BPF_RELEASE_GH_REPO} --json tagName -q .[].tagName)
+tag=$(gh release list -L 1 -R ${GH_REPO} --json tagName -q .[].tagName)
 if [[ -z "$tag" ]]; then
-    echo "Could not find latest GCC BPF release at ${GCC_BPF_RELEASE_GH_REPO}"
+    echo "Could not find latest release at ${GH_REPO}"
     exit 1
 fi
 
-url="https://github.com/${GCC_BPF_RELEASE_GH_REPO}/releases/download/${tag}/${tag}.tar.zst"
+url="https://github.com/${GH_REPO}/releases/download/${tag}/${tag}.tar.zst"
 echo "Downloading $url"
 wget -q "$url"
 
