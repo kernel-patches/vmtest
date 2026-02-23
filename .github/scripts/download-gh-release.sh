1428de9bc95330fde7f4ffa83a6aee9ac580f4ae
Ihor Solodrai <ihor.solodrai@linux.dev>
2026-02-23 15:25:48 -0800

    scripts: Factor out install-github-cli.sh
    Signed-off-by: Ihor Solodrai <ihor.solodrai@linux.dev>

--- .github/scripts/download-gh-release.sh
+++ .github/scripts/download-gh-release.sh
@@ -2,23 +2,14 @@
 
 set -euo pipefail
 
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
 GH_REPO=$1
 INSTALL_DIR=$(realpath $2)
 
 cd /tmp
 
-if ! command -v gh &> /dev/null; then
-    # https://github.com/cli/cli/blob/trunk/docs/install_linux.md
-    (type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
-	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
-	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
-	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
-	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-	&& sudo apt update \
-	&& sudo apt install gh -y
-fi
+bash "$SCRIPT_DIR/install-github-cli.sh"
 
 tag=$(gh release list -L 1 -R ${GH_REPO} --json tagName -q .[].tagName)
 if [[ -z "$tag" ]]; then
